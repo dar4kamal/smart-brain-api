70014da1da48341602ef90b7215ac466b79b63d4
dar4kamal <sasaa9646@gmail.com>
2019-03-28 03:47:45 +0200

    change pg connection

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const cors = require("cors");
 const db = require('knex')({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : '9646',
-      database : 'smart-brain'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
